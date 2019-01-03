b0eab75c6ccb1dfdb422e97695f1a783c50066da
Anguandia <mikeanguandia@gmail.com>
2019-01-03 23:27:20 +0300

    add red flag model

---
+++ app/models.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+class RedFlag:
+    def __init__(self, id, location, createdBy, comment):
+        self.id = id
+        self.location = location
+        self.createdBy = createdBy
+        self.comment = comment
